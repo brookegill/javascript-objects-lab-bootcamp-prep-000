770f9602e96baad7f0143fc2f5c69994bf218a8d
Brooke Gill <brooke.gill25@gmail.com>
2018-11-09 01:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,17 +6,20 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+
   return object;
 }
 
 function deleteFromObjectByKey(object, key) {
   var newRecipes = Object.assign({}, recipes);
   delete newRecipes[key];
+
   return newRecipes;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
   delete object[key];
+
   return object;
 }
 
@@ -26,5 +29,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
+
   return object;
 }
